6f9760b8a2ca1fa66c9972fe8289ad09324e7c9e
accidia <appleanan@outlook.com>
2026-08-22 17:12:00 +0800

    feat(brushflowlanduo): merge upstream v5.2.3

---
+++ plugins.v2/brushflowlanduo/src/components/Page.vue
@@ -0,0 +1,20 @@
+<script setup>
+import BrushFlowWorkbench from './BrushFlowWorkbench.vue'
+
+defineProps({
+  api: { type: Object, default: () => ({}) },
+})
+
+defineEmits(['action', 'close'])
+</script>
+
+<template>
+  <BrushFlowWorkbench
+    :api="api"
+    plugin-id="BrushFlowLanduo"
+    show-close
+    compact
+    @action="$emit('action')"
+    @close="$emit('close')"
+  />
+</template>
